ee262ce3959664458a3a6ebb8aaf6f050e78ba2c
Rpypro <114276492+Rpypro@users.noreply.github.com>
2024-10-23 16:00:37 +0530

    Substraction of two Integer

---
+++ Substraction.java
@@ -0,0 +1,9 @@
+public class Substraction {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 25;
+        int c = a - b;
+
+        System.out.println("Substration: " + c);
+    }
+}
