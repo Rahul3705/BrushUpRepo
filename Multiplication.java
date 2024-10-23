a95246589b0fc0faeb7b77f6c443a879a8014d0f
Rpypro <114276492+Rpypro@users.noreply.github.com>
2024-10-23 21:03:35 +0530

    Multiplication of two Integer

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 21;
+        int c = a * b;
+
+        System.out.println("Multiplication: " + c);
+    }
+}
