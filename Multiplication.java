c8f2d76581406b1026b7441886183892f8ff49ae
Rahul Waghmare <rahulwaghmare3705@gmail.com>
2025-04-10 09:38:39 +0530

    Delete Multiplication.java

--- Multiplication.java
+++
@@ -1,9 +0,0 @@
-public class Multiplication {
-    public static void main(String[] args) {
-        int a = 32;
-        int b = 21;
-        int c = a * b;
-
-        System.out.println("Multiplication: " + c);
-    }
-}
